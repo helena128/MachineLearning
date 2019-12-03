fcde098a9ea8742e591999b5474d9c091adfe537
Elena Tcheprasova <hellogbel@gmail.com>
2019-12-03 22:31:03 +0300

    Minor fixes in Task1

--- Task1/statistics_calculator.py
+++ Task1/statistics_calculator.py
@@ -13,7 +13,7 @@ with open('salary_and_population.csv', 'r') as file:
             stat_array.append(int(row[2]))
             line_count += 1
     #print(len(stat_array))
-    print('Mean: ', statistics.mean(stat_array))
-    print('Median: ', statistics.median(stat_array))
-    print('Variance: ', statistics.pvariance(stat_array))
-    print('Std dev: ', statistics.pstdev(stat_array))
+    print('Mean: ', number(statistics.mean(stat_array), 3))
+    print('Median: ', number(statistics.median(stat_array), 3))
+    print('Variance: ', number(statistics.pvariance(stat_array), 3))
+    print('Std dev: ', number(statistics.pstdev(stat_array), 3))
