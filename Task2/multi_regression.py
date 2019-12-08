ccdbac78b5ab91e219597495320a56675c84fffb
Elena Tcheprasova <hellogbel@gmail.com>
2019-12-08 17:07:52 +0300

    Added correct Task 2.2

--- Task2/multi_regression.py
+++ Task2/multi_regression.py
@@ -1,26 +1,28 @@
-import pandas as pd  
-import numpy as np  
-import matplotlib.pyplot as plt  
-import seaborn as seabornInstance 
-from sklearn.model_selection import train_test_split 
+import pandas as pd 
 from sklearn.linear_model import LinearRegression
-from sklearn import metrics
 
-dataset = pd.read_csv('candy-data.csv')
 
-X = dataset[['chocolate','fruity','caramel',
-			'peanutyalmondy','nougat','crispedricewafer',
-			'hard','bar','pluribus','sugarpercent','pricepercent']].values
-y = dataset['winpercent']
-model = LinearRegression().fit(X, y)
-y_pred = model.predict(X)
-df = pd.DataFrame({'Actual': y, 'Predicted': y_pred})
-df1 = df.head(100)
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(df)
-print(model.coef_)
-coef = model.coef_
-prediction = 0 * coef[0] + 1 * coef[1] + 1 * coef[2] + 1 * coef[3] + \
-			0 * coef[4] + 0 * coef[5] + 1 * coef[6] + 0 * coef[7] + 0 * coef[8] + \
-			0.32 * coef[9] + 0.219 * coef[10]
-print('Prediction: ', prediction)
+DATA = pd.read_csv('candy-data.csv', delimiter=',', index_col='competitorname')
+#print(DATA)
+
+# chose data for training
+train_data = DATA.drop(['Haribo Twin Snakes', 'Hersheys Krackel'])
+#print(train_data)
+
+# set predictors and Y
+X = pd.DataFrame(train_data.drop(['winpercent', 'Y'], axis = 1))
+y = pd.DataFrame(train_data['winpercent'])
+
+# train model
+reg = LinearRegression().fit(X, y)
+
+# prediction for Haribo Twin Snakes
+Haribo = DATA.loc['Haribo Twin Snakes', :].to_frame().T
+print('For Haribo: ', reg.predict(Haribo.drop(['winpercent', 'Y'], axis = 1)))
+
+# prediction for Hersheys Krackel
+Hersheys = DATA.loc['Hersheys Krackel', :].to_frame().T
+print('For Hersheys: ', reg.predict(Hersheys.drop(['winpercent', 'Y'], axis = 1)))
+
+# prediction for candy with given params
+print('For candy with given params: ', reg.predict([[0, 1, 1, 1, 0, 0, 1, 0, 0, 0.32, 0.219]]))
